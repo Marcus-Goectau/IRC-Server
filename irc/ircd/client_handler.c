b048d76ff8f1e2b703e7a16384cb35473f2948bd
Marcus Goeckner <goecmarc@isu.edu>
2020-03-11 16:56:06 -0600

    added client_handler source code and header files

---
+++ irc/ircd/client_handler.c
@@ -0,0 +1,11 @@
+/*
+ * client_handler.h
+ * CS3337
+ * Written by: Marcus Goeckner
+ * 3/10/2020
+ *
+ * Data structure to handle client data as new clients connect to the server
+*/
+
+#include "client_handler.h"
+
---
+++ irc/ircd/client_handler.h
@@ -0,0 +1,19 @@
+/*
+ * client_handler.h
+ * CS3337
+ * Written by: Marcus Goeckner
+ * 3/10/2020
+ *
+ * Header file for client handling data structure
+*/
+
+#ifndef IRCD_CLIENT_HANDLER_H
+#define IRCD_CLIENT_HANDLER_H
+
+#include <netinet/in.h>
+
+struct Client {
+    
+};
+
+#endif //IRCD_CLIENT_HANDLER_H

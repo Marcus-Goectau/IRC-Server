942783ac03096ab5a4c054cc5b4cc1b8b076a8ab
Marcus Goeckner <goecmarc@isu.edu>
2020-03-12 19:22:06 -0600

    Added channel.c and channel.h for handling channels in the server

---
+++ irc/ircd/channel.c
@@ -0,0 +1,11 @@
+/*
+ * channel.c
+ * CS3337
+ * Written by: Marcus Goeckner
+ * 3/12/2020
+ *
+ * Data structure to handle channels and their components
+*/
+
+#include "channel.h"
+
---
+++ irc/ircd/channel.h
@@ -0,0 +1,29 @@
+/*
+ * channel.h
+ * CS3337
+ * Written by: Marcus Goeckner
+ * 3/12/2020
+ *
+ * Header file for channel handling data structure
+*/
+#ifndef IRC_CHANNEL_H
+#define IRC_CHANNEL_H
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include "client_handler.h"
+#include "linked_list.h"
+
+struct Channel {
+    struct LinkedListNode *client_list_head; // List of users in this channel
+    char *name;
+    char *topic;
+    pthread_mutex_t channel_mutex; //To avoid two threads editing client_list at the same time
+};
+
+struct Channel* channel_create(char *name, char *topic);
+struct Channel* channel_remove(struct Channel *channel);
+
+struct LinkedListNode *channel_list_head;
+
+#endif //IRC_CHANNEL_H

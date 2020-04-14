d02eca857354b87cd3dc0d747c4fd4fc6244f374
Marcus Goeckner <goecmarc@isu.edu>
2020-04-13 22:36:07 -0600

    Created logger.c and logger.h to handle logging of the server.

---
+++ irc/ircd/logger.c
@@ -0,0 +1,5 @@
+//
+// Created by Marcus on 4/13/20.
+//
+
+#include "logger.h"
---
+++ irc/ircd/logger.h
@@ -0,0 +1,8 @@
+//
+// Created by Marcus on 4/13/20.
+//
+
+#ifndef IRC_LOGGER_H
+#define IRC_LOGGER_H
+
+#endif //IRC_LOGGER_H

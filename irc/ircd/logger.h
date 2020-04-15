5c0ee11e493a59e50ed12394bd37fcba5cd3c451
Marcus Goeckner <goecmarc@isu.edu>
2020-04-15 00:31:25 -0600

    Added variables and write function to be used for program logging

--- irc/ircd/logger.h
+++ irc/ircd/logger.h
@@ -1,8 +1,21 @@
-//
-// Created by Marcus on 4/13/20.
-//
+/*
+ * logger.h
+ * CS3337
+ * Written by: Marcus Goeckner
+ * 4/14/2020
+ *
+ * Header file for logging functionalities
+*/
 
 #ifndef IRC_LOGGER_H
 #define IRC_LOGGER_H
+
+#include <bits/types/FILE.h>
+
+void logger_write(char log_entry[256]);
+
+FILE *logger_log_file;
+char logger_log_file_location[256];
+int logger_logging_level; //indicates level of logging (w/ 0 = no logging)
 
 #endif //IRC_LOGGER_H

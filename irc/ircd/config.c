e4846267dc386158469b77f3bad3147416ba2f3a
Marcus Goeckner <goecmarc@isu.edu>
2020-04-15 23:00:55 -0600

    Created config.c and config.h files for parsing configuration file for server settings

---
+++ irc/ircd/config.c
@@ -0,0 +1,5 @@
+//
+// Created by Marcus on 4/15/20.
+//
+
+#include "config.h"
---
+++ irc/ircd/config.h
@@ -0,0 +1,17 @@
+/*
+ * config.h
+ * CS3337
+ * Written by: Marcus Goeckner
+ * 4/15/2020
+ *
+ * Header file for configuration file tools
+*/
+
+#ifndef IRC_CONFIG_H
+#define IRC_CONFIG_H
+
+void config_parse(char *buffer);
+
+
+
+#endif //IRC_CONFIG_H

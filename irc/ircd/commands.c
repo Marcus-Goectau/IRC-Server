9963412606245fe38c7da5e944c1ebf41883ecee
Marcus Goeckner <goecmarc@isu.edu>
2020-04-26 21:28:01 -0600

    Stubbed out functions for IRC commands

--- irc/ircd/commands.c
+++ irc/ircd/commands.c
@@ -1,5 +1,74 @@
-//
-// Created by Marcus on 4/26/20.
-//
+/*
+ * commands.c
+ * CS3337
+ * Written by: Marcus Goeckner
+ * 4/26/2020
+ *
+ * support for command messages sent by clients
+*/
 
 #include "commands.h"
+
+int commands_getCommand(char *command) {
+    return 1;
+}
+
+int commands_PASS(char *password) {
+    return 1;
+}
+
+int commands_NICK(char *nic) {
+    return 1;
+}
+
+int commands_USER(char *user_name, char *server_name, char *real_name) {
+    return 1;
+}
+
+int commands_OPER(char *user, char *password) {
+    return 1;
+}
+
+int commands_QUIT(char *message) {
+    return 1;
+}
+
+int commands_JOIN(char *channel) {
+    return 1;
+}
+
+int commands_PART(char *channel) {
+    return 1;
+}
+
+int commands_channel_MODE(char *channel, char mode) {
+    return 1;
+}
+
+int commands_user_MODE(char *nick_name, char mode) {
+    return 1;
+}
+
+int commands_TOPIC(char *channel, char *topic) {
+    return 1;
+}
+
+int commands_NAMES(char *channel) {
+    return 1;
+}
+
+int commands_LIST() {
+    return 1;
+}
+
+int commands_INVITE(char *nick_name, char *channel) {
+    return 1;
+}
+
+int commands_KICK(char *channel, char *user) {
+    return 1;
+}
+
+int commands_PRIVMSG(char *receiver, char *message) {
+    return 1;
+}
--- irc/ircd/commands.h
+++ irc/ircd/commands.h
@@ -1,8 +1,30 @@
-//
-// Created by Marcus on 4/26/20.
-//
+/*
+ * commands.h
+ * CS3337
+ * Written by: Marcus Goeckner
+ * 4/26/2020
+ *
+ * Header file for command functionalities
+*/
 
 #ifndef IRC_COMMANDS_H
 #define IRC_COMMANDS_H
+
+int commands_getCommand(char *command);
+int commands_PASS(char *password);
+int commands_NICK(char *nic);
+int commands_USER(char *user_name, char *server_name, char *real_name);
+int commands_OPER(char *user, char *password);
+int commands_QUIT(char *message);
+int commands_JOIN(char *channel);
+int commands_PART(char *channel);
+int commands_channel_MODE(char *channel, char mode);
+int commands_user_MODE(char *nick_name, char mode);
+int commands_TOPIC(char *channel, char *topic);
+int commands_NAMES(char *channel);
+int commands_LIST();
+int commands_INVITE(char *nick_name, char *channel);
+int commands_KICK(char *channel, char *user);
+int commands_PRIVMSG(char *receiver, char *message);
 
 #endif //IRC_COMMANDS_H

200b44cfb0d7be4ac12d9117e6546672b8403ed4
Marcus Goeckner <goecmarc@isu.edu>
2020-04-26 20:40:35 -0600

    Created header and source file for commands to be used in the server

---
+++ irc/ircd/commands.c
@@ -0,0 +1,5 @@
+//
+// Created by Marcus on 4/26/20.
+//
+
+#include "commands.h"
---
+++ irc/ircd/commands.h
@@ -0,0 +1,8 @@
+//
+// Created by Marcus on 4/26/20.
+//
+
+#ifndef IRC_COMMANDS_H
+#define IRC_COMMANDS_H
+
+#endif //IRC_COMMANDS_H
